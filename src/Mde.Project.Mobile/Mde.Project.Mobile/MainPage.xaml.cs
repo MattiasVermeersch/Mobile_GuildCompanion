e307c4098052ea4675d6814293a1ec997f53504f
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-10-27 22:35:05 +0200

    Update MainPage to a Flyout Page

--- src/Mde.Project.Mobile/Mde.Project.Mobile/MainPage.xaml.cs
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/MainPage.xaml.cs
@@ -8,7 +8,7 @@ using Xamarin.Forms;
 
 namespace Mde.Project.Mobile
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : FlyoutPage
     {
         public MainPage()
         {
