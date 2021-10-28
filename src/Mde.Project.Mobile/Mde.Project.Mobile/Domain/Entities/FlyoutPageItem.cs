78dd9800ffcfcfc3ee759aa298a0afa31c4b214b
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-10-28 22:40:53 +0200

    Create FlyoutPageItem class

---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Entities/FlyoutPageItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mde.Project.Mobile
+{
+    public class FlyoutPageItem
+    {
+        public string Title { get; set; }
+        public string IconSource { get; set; }
+        public Type TargetTypePage { get; set; }
+    }
+}
