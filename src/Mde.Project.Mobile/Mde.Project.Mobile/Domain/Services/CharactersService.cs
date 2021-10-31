ebe4ce5c5cdf8a1636dd60d27c487cdfe16ce2b1
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-10-31 23:55:39 +0100

    Add CharacterService

---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Services/CharactersService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mde.Project.Mobile.Domain.Services
+{
+    public class CharactersService
+    {
+        private List<string> characters;
+
+        public List<string> GetCharacters()
+        {
+            characters = new List<string>
+            {
+                "Character 1",
+                "Character 2",
+                "Character 3"
+            };
+
+            return characters;
+        }
+    }
+}
