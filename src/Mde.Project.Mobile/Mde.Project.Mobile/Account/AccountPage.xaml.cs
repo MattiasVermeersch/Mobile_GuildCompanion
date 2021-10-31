ffd81e5fe87efce277c7bd0fc0de1d38be30ec92
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-10-31 23:45:54 +0100

    Move AccountPage to seperate folder

--- src/Mde.Project.Mobile/Mde.Project.Mobile/Account/AccountPage.xaml.cs
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Account/AccountPage.xaml.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace Mde.Project.Mobile
+namespace Mde.Project.Mobile.Account
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AccountPage : ContentPage
