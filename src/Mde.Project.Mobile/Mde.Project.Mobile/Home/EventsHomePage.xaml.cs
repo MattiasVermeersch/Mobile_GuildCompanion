7f18e22106a73a9d95a4d167ed613af76eea3989
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-10-31 23:46:41 +0100

    Move EventsHomePage to seperate folder

---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Home/EventsHomePage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Mde.Project.Mobile.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mde.Project.Mobile.Home
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+
+    
+    public partial class EventsHomePage : ContentPage
+    {
+        private readonly EventsService eventsService;
+        
+        public EventsHomePage()
+        {
+            InitializeComponent();
+
+            eventsService = new EventsService();
+
+            homePveEventsList.ItemsSource = eventsService.GetPveEvents();
+            homePvpEventsList.ItemsSource = eventsService.GetPvpEvents();
+        }
+
+        protected override void OnAppearing()
+        {
+            
+        }
+    }
+}
