1f789ed135ea1dcd9556ab4bb18e55c4bd9761db
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-11-04 20:20:02 +0100

    Create RegisterPage

---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Login/RegisterPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mde.Project.Mobile.Login
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegisterPage : ContentPage
+    {
+        public RegisterPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnSubmit_Clicked(object sender, EventArgs e)
+        {
+            if(string.IsNullOrWhiteSpace(txtEmail.Text) || 
+               string.IsNullOrWhiteSpace(txtName.Text) ||
+               string.IsNullOrWhiteSpace(txtPassword.Text) ||
+               string.IsNullOrWhiteSpace(txtConfirmPassword.Text))
+            {
+                await DisplayAlert("Form error", "Please fill in the form!", "OK");
+            }
+            else if(txtPassword.Text != txtConfirmPassword.Text)
+            {
+                await DisplayAlert("Password error", "The passwords don't match!", "Let's fix this");
+            }
+            else
+            {
+                await Navigation.PopModalAsync();
+            }
+        }
+
+        private async void btnCancel_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
+}
