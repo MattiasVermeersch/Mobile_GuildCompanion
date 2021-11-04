39082959925a1fd1d20007fc085883525d60fdf2
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-11-04 20:14:05 +0100

    Create LoginPage

---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Login/LoginPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Mde.Project.Mobile.Home;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mde.Project.Mobile.Login
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new RegisterPage());
+        }
+
+        private async void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync($"//{nameof(EventsHomePage)}");
+        }
+    }
+}
