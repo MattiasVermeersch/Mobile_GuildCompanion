fb31fc220359bb65ab1894951ffc381eda98687d
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-11-06 23:47:18 +0100

    Update AccountPage to receive dummydata

--- src/Mde.Project.Mobile/Mde.Project.Mobile/Account/AccountPage.xaml.cs
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Account/AccountPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Mde.Project.Mobile.Domain.Models;
+using Mde.Project.Mobile.Domain.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,26 @@ namespace Mde.Project.Mobile.Account
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AccountPage : ContentPage
     {
+        private readonly AccountService _accountService;
+        private DummyProfile profile;
         public AccountPage()
         {
             InitializeComponent();
+
+            _accountService = new AccountService();
+        }
+
+        protected async override void OnAppearing()
+        {
+            profile = await _accountService.GetDummyProfile();
+            txtName.Text = profile.Name;
+            txtEmail.Text = profile.Email;
+            txtDiscordId.Text = profile.DiscordID;
+        }
+
+        private void btnEdit_Clicked(object sender, EventArgs e)
+        {
+            //Does nothing yet.
         }
     }
 }
