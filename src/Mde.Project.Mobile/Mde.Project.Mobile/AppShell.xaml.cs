3830aa43ccdf610b23c129201508a735416c6731
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-11-28 19:44:21 +0100

    Update AppShell Routing

--- src/Mde.Project.Mobile/Mde.Project.Mobile/AppShell.xaml.cs
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/AppShell.xaml.cs
@@ -19,6 +19,7 @@ namespace Mde.Project.Mobile
 
             #region Routing
             Routing.RegisterRoute(nameof(LoginPage), typeof(LoginPage));
+            Routing.RegisterRoute(nameof(RegisterPage), typeof(RegisterPage));
             Routing.RegisterRoute(nameof(HomePage), typeof(HomePage));
             Routing.RegisterRoute(nameof(EventsPage), typeof(EventsPage));
             Routing.RegisterRoute(nameof(CharactersPage), typeof(CharactersPage));
