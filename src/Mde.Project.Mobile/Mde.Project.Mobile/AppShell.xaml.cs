deb8a070ae6c586850a9da3d970ede87b6dac27b
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-11-28 16:39:58 +0100

    Add Routing to AppShell

--- src/Mde.Project.Mobile/Mde.Project.Mobile/AppShell.xaml.cs
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/AppShell.xaml.cs
@@ -16,6 +16,15 @@ namespace Mde.Project.Mobile
         public AppShell()
         {
             InitializeComponent();
+
+            #region Routing
+            Routing.RegisterRoute(nameof(LoginPage), typeof(LoginPage));
+            Routing.RegisterRoute(nameof(HomePage), typeof(HomePage));
+            Routing.RegisterRoute(nameof(EventsPage), typeof(EventsPage));
+            Routing.RegisterRoute(nameof(CharactersPage), typeof(CharactersPage));
+            Routing.RegisterRoute(nameof(AccountPage), typeof(AccountPage));
+            #endregion
+
         }
 
         private async void MenuItem_Clicked(object sender, EventArgs e)
