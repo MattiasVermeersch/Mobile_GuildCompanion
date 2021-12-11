b5f28af400e07fb0d81962a9e5efa556bbe98f44
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-12-11 20:06:46 +0100

    Create ModelBase

---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Models/Base/ModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mde.Project.Mobile.Domain.Models
+{
+    public abstract class ModelBase
+    {
+        public string Id { get; set; }
+    }
+}
