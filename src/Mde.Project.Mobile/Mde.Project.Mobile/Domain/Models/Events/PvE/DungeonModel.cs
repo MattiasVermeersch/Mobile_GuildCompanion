bd7b9aa754d4df07524defc9e4f726896041d5a5
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-12-11 20:11:47 +0100

    Create DungeonModel

---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Models/Events/PvE/DungeonModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mde.Project.Mobile.Domain.Models
+{
+    public class DungeonModel : EventModelBase
+    {
+        public string Difficulty { get; set; }
+        public int? Key { get; set; }
+        public bool Success { get; set; }
+    }
+}
