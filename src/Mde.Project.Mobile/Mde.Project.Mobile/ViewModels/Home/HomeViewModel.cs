96575ca90947804d61d3fcde010fcb90d3772bab
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-12-11 22:50:35 +0100

    Create HomeViewModel

---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/ViewModels/Home/HomeViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mde.Project.Mobile.ViewModels
+{
+    public class HomeViewModel : ViewModelBase
+    {
+        public HomeViewModel()
+        {
+
+        }
+
+        #region Properties
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+    }
+}
