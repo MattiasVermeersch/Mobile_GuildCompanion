6070eaa74b35c30213e065c149d28b00d00e4b8c
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-12-11 23:15:43 +0100

    Create IoCResolver with Autofac

---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/IoC/IoCResolver.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+
+namespace Mde.Project.Mobile.IoC
+{
+    public class IoCResolver
+    {
+        private static IContainer container;
+        public static IContainer Container
+        {
+            get
+            {
+                if(container == null)
+                {
+                    var builder = IoCBuilder.GetContainerBuilder();
+                    container = builder.Build();
+                }
+                return container;
+            }
+        }
+    }
+}
