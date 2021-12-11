97d783d35df4a4e99ea352d6d1b52bedbc3bc060
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-12-11 22:33:34 +0100

    Update DungeonsPage code behind

--- src/Mde.Project.Mobile/Mde.Project.Mobile/Pages/Events/DungeonsPage.xaml.cs
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Pages/Events/DungeonsPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mde.Project.Mobile.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,8 @@ namespace Mde.Project.Mobile.Pages
         public DungeonsPage()
         {
             InitializeComponent();
+
+            (BindingContext as DungeonViewModel).RefreshCommand.ExecuteAsync();
         }
     }
 }
