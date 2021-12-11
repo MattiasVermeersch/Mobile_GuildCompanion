10e1b708cffdc162ed1c5ae30403ec745af80127
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-12-11 21:01:07 +0100

    Update ViewModelBase

--- src/Mde.Project.Mobile/Mde.Project.Mobile/ViewModels/Base/ViewModelBase.cs
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/ViewModels/Base/ViewModelBase.cs
@@ -1,11 +1,27 @@
 ﻿using MvvmHelpers;
+using MvvmHelpers.Commands;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Mde.Project.Mobile.ViewModels
 {
     public class ViewModelBase : BaseViewModel
     {
+        public AsyncCommand RefreshCommand { get; }
+
+        public ViewModelBase()
+        {
+            RefreshCommand = new AsyncCommand(GetData);
+        }
+        public virtual async Task GetData()
+        {
+#if DEBUG
+            await Task.Delay(1000);
+#endif
+        }
     }
 }
