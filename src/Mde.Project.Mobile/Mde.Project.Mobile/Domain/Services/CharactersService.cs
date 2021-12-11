ad6f1fb8594c812398a34d0a2384fcb6e4159cc2
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-12-11 20:42:23 +0100

    Update CharactersService

--- src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Services/CharactersService.cs
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Services/CharactersService.cs
@@ -1,23 +1,94 @@
-﻿using System;
+﻿using Mde.Project.Mobile.Domain.Interfaces;
+using Mde.Project.Mobile.Domain.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Mde.Project.Mobile.Domain.Services
 {
-    public class CharactersService
+    public class CharactersService : ICharactersService
     {
-        private List<string> characters;
-
-        public List<string> GetCharacters()
+        private static List<CharacterModel> characters = new List<CharacterModel>
         {
-            characters = new List<string>
+            new CharacterModel
             {
-                "Character 1",
-                "Character 2",
-                "Character 3"
-            };
+                Id = "1",
+                Name = "Aerenthil",
+                Class = "Demon Hunter",
+                Level = 60,
+                ItemLevel = 238,
+                ArenaRating =  0,
+                BattlegroundRating = 0,
+                UserId = "1"
+            },
+            new CharacterModel
+            {
+                Id = "2",
+                Name = "Ghortak",
+                Class = "Death Knight",
+                Level = 60,
+                ItemLevel = 243,
+                ArenaRating =  0,
+                BattlegroundRating = 0,
+                UserId = "1"
+            },
+            new CharacterModel
+            {
+                Id = "3",
+                Name = "Bauglir",
+                Class = "Druid",
+                Level = 60,
+                ItemLevel = 238,
+                ArenaRating =  1425,
+                BattlegroundRating = 850,
+                UserId = "2"
+            },
+            new CharacterModel
+            {
+                Id = "4",
+                Name = "Liutasil",
+                Class = "Paladin",
+                Level = 60,
+                ItemLevel = 170,
+                ArenaRating =  0,
+                BattlegroundRating = 192,
+                UserId = "2"
+            },
+            new CharacterModel
+            {
+                Id = "5",
+                Name = "Yoriko",
+                Class = "Shaman",
+                Level = 60,
+                ItemLevel = 239,
+                ArenaRating =  2458,
+                BattlegroundRating = 1608,
+                UserId = "3"
+            },
+            new CharacterModel
+            {
+                Id = "6",
+                Name = "Elen",
+                Class = "Warlock",
+                Level = 60,
+                ItemLevel = 170,
+                ArenaRating =  0,
+                BattlegroundRating = 0,
+                UserId = "3",
+                IsDeleted = true
+            }
+        };
 
-            return characters;
+        public Task<IEnumerable<CharacterModel>> GetCharacters()
+        {
+            return Task.FromResult(characters.AsEnumerable());
+        }
+
+        public async Task<CharacterModel> GetCharacterById(string id)
+        {
+            return (await GetCharacters()).FirstOrDefault(c => c.Id.Equals(id));
         }
     }
 }
