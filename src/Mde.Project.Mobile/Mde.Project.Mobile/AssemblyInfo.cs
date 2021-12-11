0354d375e6d0e4e1a49c4762677aa3f402fef385
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-12-11 23:27:54 +0100

    Remove Dependency Service assembly

--- src/Mde.Project.Mobile/Mde.Project.Mobile/AssemblyInfo.cs
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/AssemblyInfo.cs
@@ -4,11 +4,3 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-
-#region Dependendy Injection
-[assembly: Dependency(typeof(CharactersService))]
-[assembly: Dependency(typeof(ArenaService))]
-[assembly: Dependency(typeof(BattlegroundService))]
-[assembly: Dependency(typeof(DungeonService))]
-[assembly: Dependency(typeof(RaidService))]
-#endregion
