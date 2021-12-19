ff634388e74c9f3dea5947daf0d738e8138eeee7
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-12-19 20:15:29 +0100

    Update HomePage and code behind with HomeViewModel

--- src/Mde.Project.Mobile/Mde.Project.Mobile/Pages/Home/HomePage.xaml.cs
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Pages/Home/HomePage.xaml.cs
@@ -1,33 +1,29 @@
-﻿using Mde.Project.Mobile.Domain.Services;
+﻿using Autofac;
+using Mde.Project.Mobile.Domain.Services;
+using Mde.Project.Mobile.IoC;
+using Mde.Project.Mobile.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Mde.Project.Mobile.Pages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-
-    
     public partial class HomePage : ContentPage
-    {
-        private readonly EventsService _eventsService;
-        
+    {       
         public HomePage()
         {
             InitializeComponent();
 
-            _eventsService = new EventsService();
-        }
+            var viewModel = IoCResolver.Container.Resolve<HomeViewModel>();
 
-        protected override void OnAppearing()
-        {
-            homePveEventsList.ItemsSource = _eventsService.GetPveEvents();
-            homePvpEventsList.ItemsSource = _eventsService.GetPvpEvents();
+            BindingContext = viewModel;
+
+            viewModel.RefreshCommand.ExecuteAsync();
         }
     }
 }
