2f358c884b2d1166c0dc2c088be8a0d876054d2d
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-12-19 20:18:29 +0100

    Update AccountPage and code behind with AccountViewModel

--- src/Mde.Project.Mobile/Mde.Project.Mobile/Pages/Account/AccountPage.xaml.cs
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Pages/Account/AccountPage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using Mde.Project.Mobile.Domain.Models;
+﻿using Autofac;
+using Mde.Project.Mobile.Domain.Models;
 using Mde.Project.Mobile.Domain.Services;
+using Mde.Project.Mobile.IoC;
+using Mde.Project.Mobile.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,70 +17,15 @@ namespace Mde.Project.Mobile.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AccountPage : ContentPage
     {
-        private readonly AccountService _accountService;
-        private DummyProfile profile;
         public AccountPage()
         {
             InitializeComponent();
 
-            _accountService = new AccountService();
-        }
+            var viewModel = IoCResolver.Container.Resolve<AccountViewModel>();
 
-        protected async override void OnAppearing()
-        {
-            profile = await _accountService.GetProfile();
-            
-            //Fill labels with account info
-            lblName.Text = profile.Name;
-            lblEmail.Text = profile.Email;
-            lblDiscordId.Text = profile.DiscordID;
+            BindingContext = viewModel;
 
-            //Fill entries with account info
-            txtName.Text = profile.Name;
-            txtEmail.Text = profile.Email;
-            txtDiscordId.Text = profile.DiscordID;
-
-            SwitchVisibility();
-        }
-
-        private void btnEdit_Clicked(object sender, EventArgs e)
-        {
-            SwitchVisibility();
-        }
-
-        private async void btnSave_Clicked(object sender, EventArgs e)
-        {
-            DummyProfile newProfile = new DummyProfile
-            {
-                Name = txtName.Text,
-                Email = txtEmail.Text,
-                DiscordID = txtDiscordId.Text
-            };
-
-            await _accountService.SaveProfile(newProfile);
-
-            OnAppearing();
-        }
-
-        private void btnCancel_Clicked(object sender, EventArgs e)
-        {
-            SwitchVisibility();
-        }
-
-        private void SwitchVisibility()
-        {
-            //Set labels with info and edit button to invisible
-            lblName.IsVisible = !lblName.IsVisible;
-            lblEmail.IsVisible = !lblEmail.IsVisible;
-            lblDiscordId.IsVisible = !lblDiscordId.IsVisible;
-            btnEdit.IsVisible = !btnEdit.IsVisible;
-
-            //Make Entries and save/cancel button visible
-            txtName.IsVisible = !txtName.IsVisible;
-            txtEmail.IsVisible = !txtEmail.IsVisible;
-            txtDiscordId.IsVisible = !txtDiscordId.IsVisible;
-            btnSave.IsVisible = !btnSave.IsVisible;
-            btnCancel.IsVisible = !btnCancel.IsVisible;
+            viewModel.RefreshCommand.ExecuteAsync();
         }
     }
 }
