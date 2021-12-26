76096bac47bb639afd1f70908ac7a14557e3169e
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-12-26 16:57:55 +0100

    Create AuthService

---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Services/API/AuthService.cs
@@ -0,0 +1,66 @@
+﻿using Mde.Project.Mobile.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Mde.Project.Mobile.Domain.Models;
+using Xamarin.Essentials;
+using Mde.Project.Mobile.Domain.Constants;
+
+namespace Mde.Project.Mobile.Domain.Services.API
+{
+    public class AuthService : IAuthService
+    {
+        private const string _baseUrl = "https://localhost:5001/api/auth";
+        private readonly HttpClient client;
+
+        public AuthService()
+        {
+            client = new HttpClient(GetClientHandler());
+        }
+
+        public async Task<bool> Login(string username, string password)
+        {
+            try
+            {
+                var loginRequest = new
+                {
+                    username,
+                    password
+                };
+
+                var response = await client.PostAsJsonAsync(_baseUrl, loginRequest);
+                var json = await response.Content.ReadAsStringAsync();
+
+                var authResult = JsonConvert.DeserializeObject<LoginModel>(json);
+
+                if (authResult.IsSignedIn) 
+                    await SecureStorage.SetAsync(ApiConstants.TokeyKey, authResult.Token);
+
+                return authResult.IsSignedIn;
+            }
+            catch
+            {
+                throw;
+            }
+            
+        }
+
+        public Task<string> GetToken()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public HttpMessageHandler GetClientHandler()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsAuthenticated()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
