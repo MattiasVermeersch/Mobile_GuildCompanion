53939cc51c039617b4d778fd6f235845ba7306f2
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-12-26 16:47:40 +0100

    Move current services to seperate Mock folder

---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Interfaces/MockServices/ICharactersService.cs
@@ -0,0 +1,12 @@
+﻿using Mde.Project.Mobile.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Mde.Project.Mobile.Domain.Interfaces
+{
+    public interface ICharactersService
+    {
+        Task<CharacterModel> GetCharacterById(string characterId);
+        Task<IEnumerable<CharacterModel>> GetCharacters();
+    }
+}
