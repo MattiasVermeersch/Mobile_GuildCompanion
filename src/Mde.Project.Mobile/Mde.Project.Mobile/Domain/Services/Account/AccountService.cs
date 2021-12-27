93ea03f1683fcff065760307ef32b1191b7bc25f
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-12-27 22:02:38 +0100

    Create AccountService

--- src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Services/Account/AccountService.cs
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Services/Account/AccountService.cs
@@ -1,14 +1,21 @@
 ﻿using Mde.Project.Mobile.Domain.Interfaces;
 using Mde.Project.Mobile.Domain.Models;
+using Mde.Project.Mobile.Domain.Constants;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Newtonsoft.Json;
+using System.Diagnostics;
 
 namespace Mde.Project.Mobile.Domain.Services
 {
     public class AccountService : IAccountService
     {
+        protected static string fullPath = Path.Combine(FileSystem.AppDataDirectory, AppConstants.AccountSettingsFileName);
+
         private static AccountModel dummyAccount = new AccountModel
         {
             UserName = "ageydon0@marriott.com",
@@ -17,17 +24,38 @@ namespace Mde.Project.Mobile.Domain.Services
             BirthDate = new DateTime(1993, 02, 05),
             Address = "6 Gale Plaza",
             FullName = "Aldwin Geydon",
-            BattleNetId = "Alderaan#2208"
+            BattleNetId = "Alderaan#2208",
+            AvatarPath = "default_avatar_icon.png"
         };
 
         public async Task<AccountModel> GetProfile()
         {
-            return await Task.FromResult(dummyAccount);
+            //check if account settings file exists
+            if (File.Exists(fullPath))
+            {
+                string json = File.ReadAllText(fullPath);
+                var accountSettings = JsonConvert.DeserializeObject<AccountModel>(json);
+                return await Task.FromResult(accountSettings);
+                
+            }
+            else
+            {
+                return await Task.FromResult(dummyAccount);
+            }
+           
         }
 
-        public async Task SaveProfile(AccountModel profile)
+        public Task<bool> SaveProfile(AccountModel account)
         {
-            dummyAccount = await Task.FromResult(profile);
+            var json = JsonConvert.SerializeObject(account, 
+                new JsonSerializerSettings()
+                {
+                    ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+                });
+            
+            File.WriteAllText(fullPath, json);
+
+            return Task.FromResult(true);
         }
     }
 }
