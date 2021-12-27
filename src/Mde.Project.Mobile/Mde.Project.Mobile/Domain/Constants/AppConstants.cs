8d01b1b7c9c7a432c240f3934648283e2eeaa8e1
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-12-27 22:02:28 +0100

    Create AppConstants class

---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Constants/AppConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mde.Project.Mobile.Domain.Constants
+{
+    public static class AppConstants
+    {
+        public static string AccountSettingsFileName = "accountsettings.json";
+    }
+}
