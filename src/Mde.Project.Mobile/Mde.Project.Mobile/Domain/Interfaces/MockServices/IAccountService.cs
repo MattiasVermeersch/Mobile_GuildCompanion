b64f43eaf944ffdb981437697c68d93f8f103d2f
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-12-27 22:02:16 +0100

    Move Services Interfaces to seperate folder

--- src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Interfaces/MockServices/IAccountService.cs
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Interfaces/MockServices/IAccountService.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using Mde.Project.Mobile.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Mde.Project.Mobile.Domain.Interfaces
 {
     public interface IAccountService
     {
-
+        Task<AccountModel> GetProfile();
+        Task<bool> SaveProfile(AccountModel profile);
     }
 }
