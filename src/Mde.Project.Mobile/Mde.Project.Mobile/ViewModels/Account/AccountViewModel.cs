e26012e1a42a273ffe24c063aeb3abcabdba05fa
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-12-27 22:03:29 +0100

    Update AccountViewModel with MediaPicker

--- src/Mde.Project.Mobile/Mde.Project.Mobile/ViewModels/Account/AccountViewModel.cs
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/ViewModels/Account/AccountViewModel.cs
@@ -1,9 +1,13 @@
 ﻿using Mde.Project.Mobile.Domain.Interfaces;
 using Mde.Project.Mobile.Domain.Models;
+using MvvmHelpers.Commands;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
 
 namespace Mde.Project.Mobile.ViewModels
 {
@@ -14,6 +18,9 @@ namespace Mde.Project.Mobile.ViewModels
         {
             _accountService = accountService;
             Title = "My Account";
+
+            SaveProfileCommand = new AsyncCommand(SaveProfileAsync);
+            TakePhotoCommand = new AsyncCommand(TakePhotoAsync);
         }
 
         #region Properties
@@ -65,6 +72,17 @@ namespace Mde.Project.Mobile.ViewModels
             get => battleNetId;
             set => SetProperty(ref battleNetId, value);
         }
+
+        ImageSource profilePicture;
+        public ImageSource ProfilePicture
+        {
+            get => profilePicture;
+            set => SetProperty(ref profilePicture, value);
+        }
+
+        public AsyncCommand SaveProfileCommand { get; }
+        public AsyncCommand TakePhotoCommand { get; }
+       
         #endregion
 
         #region Commands
@@ -83,6 +101,10 @@ namespace Mde.Project.Mobile.ViewModels
         {
             var account = await _accountService.GetProfile();
 
+            //FileResult photo = new FileResult(account.AvatarPath);
+
+            //await LoadPhotoAsync(photo);
+
             if (account != null) SetProperties(account);
         }
 
@@ -95,6 +117,77 @@ namespace Mde.Project.Mobile.ViewModels
             Address = account.Address;
             FullName = account.FullName;
             BattleNetId = account.BattleNetId;
+            ProfilePicture = (ImageSource)account.AvatarPath;
+        }
+        async Task SaveProfileAsync()
+        {
+            AccountModel currentAccount = new AccountModel
+            {
+                UserName = UserName,
+                Email = Email,
+                City = City,
+                BirthDate = BirthDate,
+                Address = Address,
+                FullName = FullName,
+                BattleNetId = BattleNetId,
+                AvatarPath = ProfilePicture.ToString()
+            };
+
+            if(await _accountService.SaveProfile(currentAccount))
+            {
+                await Shell.Current.DisplayAlert("Success", "Your account info has been saved.", "OK");
+            }
+            else
+            {
+                await Shell.Current.DisplayAlert("Error", "Your account info has not been saved, try again", "OK");
+            }
+        }
+
+        async Task TakePhotoAsync()
+        {
+            try
+            {
+                var photo = await MediaPicker.CapturePhotoAsync();
+
+                await LoadPhotoAsync(photo);
+
+                await Shell.Current.DisplayAlert("Success", "Your new profile picture is taken.", "OK");
+            }
+
+            catch (FeatureNotSupportedException fnsEx) 
+            { 
+                await Shell.Current.DisplayAlert("Error", $"{fnsEx.Message}", "OK"); 
+            }
+
+            catch (PermissionException psEx) 
+            {
+                await Shell.Current.DisplayAlert("Error", $"{psEx.Message}", "OK");
+            }
+            
+            catch (Exception ex)
+            {
+                await Shell.Current.DisplayAlert("Error", $"{ex.Message}", "OK");
+            }
+        }
+
+        async Task LoadPhotoAsync(FileResult photo)
+        {
+            //when canceled
+            if(photo == null)
+            {
+                return;
+            }
+
+            //save the image into local storage
+            var newFile = Path.Combine(FileSystem.AppDataDirectory, photo.FileName);
+
+            using (var stream = await photo.OpenReadAsync())
+            using (var newStream = File.OpenWrite(newFile))
+                await stream.CopyToAsync(newStream);
+
+            ProfilePicture = newFile;
+
+            await SaveProfileAsync();
         }
     }
 }
