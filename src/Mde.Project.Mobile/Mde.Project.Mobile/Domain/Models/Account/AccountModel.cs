e88e97232610612504f348e1691ab3009f5d3b6d
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-02 21:23:49 +0100

    Update AccountModel

--- src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Models/Account/AccountModel.cs
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Models/Account/AccountModel.cs
@@ -8,8 +8,6 @@ namespace Mde.Project.Mobile.Domain.Models
 {
     public class AccountModel : ModelBase
     {
-        [DataType(DataType.Text)]
-        public string UserName { get; set; }
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
         public string City { get; set; }
@@ -17,7 +15,6 @@ namespace Mde.Project.Mobile.Domain.Models
         public string Address { get; set; }
         public string FullName { get; set; }
         public string BattleNetId { get; set; }
-        public string AvatarPath { get; set; }
     }
 }
 
