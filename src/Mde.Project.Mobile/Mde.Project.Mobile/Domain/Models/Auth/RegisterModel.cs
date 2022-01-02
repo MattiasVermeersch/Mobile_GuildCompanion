ef02ebbe7ca8209db5fde0134c30f4ecf7d77851
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-02 21:30:09 +0100

    Create RegisterModel

---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Models/Auth/RegisterModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mde.Project.Mobile.Domain.Models
+{
+    public class RegisterModel
+    {
+        public string Error { get; set; }
+    }
+}
