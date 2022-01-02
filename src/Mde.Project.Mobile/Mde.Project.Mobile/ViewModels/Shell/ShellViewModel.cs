891dd8bae45fadd7ae631ff0bcabcd33ef9a5625
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-02 21:29:09 +0100

    Create ViewModel for AppShell

---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/ViewModels/Shell/ShellViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Mde.Project.Mobile.Domain.Interfaces;
+using Mde.Project.Mobile.Pages;
+using MvvmHelpers.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Mde.Project.Mobile.ViewModels
+{
+    public class ShellViewModel : ViewModelBase
+    {
+        protected readonly IAuthService _authService;
+        public ShellViewModel(IAuthService authService)
+        {
+            _authService = authService;
+
+            //Commands
+            LogoutCommand = new AsyncCommand(Logout);
+        }
+
+        public ICommand LogoutCommand { get; }
+
+        async Task Logout()
+        {
+           _authService.Logout();
+            await AppShell.Current.GoToAsync($"//{nameof(LoginPage)}");
+        }
+    }
+}
