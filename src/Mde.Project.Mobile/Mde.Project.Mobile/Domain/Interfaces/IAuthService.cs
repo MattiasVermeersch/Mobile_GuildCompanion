612ed6aa73522ac3fb59da438f40b8411b72507f
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-02 21:18:32 +0100

    Move and Update IAuthService

--- src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Interfaces/IAuthService.cs
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Interfaces/IAuthService.cs
@@ -10,5 +10,9 @@ namespace Mde.Project.Mobile.Domain.Interfaces
         Task<bool> Login(string username, string password);
         Task<string> GetToken();
         Task<bool> IsAuthenticated();
+        void Logout();
+        Task<bool> RegisterNewUser(string firstName, string lastName,
+            string email, string city, string address, DateTime birthDate, 
+            string password, string confirmPassword);
     }
 }
