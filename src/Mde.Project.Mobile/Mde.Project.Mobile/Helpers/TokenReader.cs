310bf8c5a0a3afbaf8ae26ad0b7573af682e5e3a
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-02 21:17:28 +0100

    Create TokenReader class

---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Helpers/TokenReader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+
+namespace Mde.Project.Mobile.Helpers
+{
+    public class TokenReader
+    {
+        public string GetUserName(string rawToken)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.ReadJwtToken(rawToken);
+            return token.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+        }
+
+        public bool IsExpired(string rawToken)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.ReadJwtToken(rawToken);
+            return token.ValidTo < DateTime.Now;
+        }
+    }
+}
