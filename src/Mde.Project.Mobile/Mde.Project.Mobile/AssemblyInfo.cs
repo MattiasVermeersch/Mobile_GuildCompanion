d1d782bc804a82893d218b28a4f60a4a8db50574
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-02 21:20:41 +0100

    Update Assembly for fonts usage

--- src/Mde.Project.Mobile/Mde.Project.Mobile/AssemblyInfo.cs
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/AssemblyInfo.cs
@@ -4,3 +4,7 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("fa-brands-400.ttf", Alias = "FAB")]
+[assembly: ExportFont("fa-regular-400.ttf", Alias = "FAR")]
+[assembly: ExportFont("fa-solid-900.ttf", Alias = "FAS")]
