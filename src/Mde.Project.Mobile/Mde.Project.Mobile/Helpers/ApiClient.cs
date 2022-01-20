fcd1a571793c2598f4053b45f170c2c256cfdd8b
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-20 23:05:33 +0100

    Update ApiClient to use token

--- src/Mde.Project.Mobile/Mde.Project.Mobile/Helpers/ApiClient.cs
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Helpers/ApiClient.cs
@@ -1,9 +1,13 @@
-﻿using Newtonsoft.Json;
+﻿using Mde.Project.Mobile.Domain.Constants;
+using Mde.Project.Mobile.Domain.Interfaces;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 
 namespace Mde.Project.Mobile.Helpers
 {
@@ -19,19 +23,29 @@ namespace Mde.Project.Mobile.Helpers
 #endif
             return httpClientHandler;
         }
-        public static async Task<T> GetAsync<T>(string url)
+        public static async Task<T> GetAsync<T>(string url, bool useToken = true)
         {
             using(HttpClient client = new HttpClient(GetClientHandler()))
             {
+                if(useToken)
+                {
+                    await SetJWTHeaderForClient(client);
+                }
+
                 string response = await client.GetStringAsync(url);
                 return JsonConvert.DeserializeObject<T>(response);
             }
         }
 
-        public static async Task<T> PostAsync<T>(string url, object data)
+        public static async Task<T> PostAsync<T>(string url, object data, bool useToken = true)
         {
             using(HttpClient client = new HttpClient(GetClientHandler()))
             {
+                if (useToken)
+                {
+                    await SetJWTHeaderForClient(client);
+                }
+
                 var response = await client.PostAsJsonAsync(url, data);
                 var json = await response.Content.ReadAsStringAsync();
 
@@ -41,10 +55,15 @@ namespace Mde.Project.Mobile.Helpers
             }
         }
 
-        public static async Task<T> PutAsync<T>(string url, object data)
+        public static async Task<T> PutAsync<T>(string url, object data, bool useToken = true)
         {
             using (HttpClient client = new HttpClient(GetClientHandler()))
             {
+                if (useToken)
+                {
+                    await SetJWTHeaderForClient(client);
+                }
+
                 var response = await client.PutAsJsonAsync(url, data);
                 var json = await response.Content.ReadAsStringAsync();
 
@@ -54,10 +73,15 @@ namespace Mde.Project.Mobile.Helpers
             }
         }
 
-        public static async Task<T> DeleteAsync<T>(string url)
+        public static async Task<T> DeleteAsync<T>(string url, bool useToken = true)
         {
             using (HttpClient client = new HttpClient(GetClientHandler()))
             {
+                if (useToken)
+                {
+                    await SetJWTHeaderForClient(client);
+                }
+
                 var response = await client.DeleteAsync(url);
                 var json = await response.Content.ReadAsStringAsync();
 
@@ -65,6 +89,17 @@ namespace Mde.Project.Mobile.Helpers
 
                 return result;
             }
+        }
+
+        private static async Task<string> GetToken()
+        {
+            return await SecureStorage.GetAsync(ApiConstants.TokenKey);
+        }
+
+        private static async Task SetJWTHeaderForClient(HttpClient client)
+        {
+            var authHeader = new AuthenticationHeaderValue("Bearer", await GetToken());
+            client.DefaultRequestHeaders.Authorization = authHeader;
         }
     }
 }
