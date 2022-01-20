dff0477fcd95020d5d0e15ddee841c51c8750bf9
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-20 23:10:21 +0100

    Create BaseEventService class

---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Services/ApiServices/Base/BaseEventService.cs
@@ -0,0 +1,21 @@
+﻿using Mde.Project.Mobile.Domain.Constants;
+using Mde.Project.Mobile.Domain.Interfaces;
+using Mde.Project.Mobile.Domain.Models;
+using Mde.Project.Mobile.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mde.Project.Mobile.Domain.Services
+{
+    public class BaseEventService<T> : BaseService<T>, IEventService<T>
+        where T : EventModelBase
+    {
+        public async Task<T> AddCharacterToInstance(string id, CharacterModel character)
+        {
+            var outEntity = await ApiClient.PutAsync<T>($"{_url}/{id}/{ApiConstants.Character}", character);
+            return outEntity;
+        }
+    }
+}
