7943e285932813cd2444def67ef5dcca398472a7
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-20 23:08:51 +0100

    Create DungeonService interface

---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Interfaces/Events/IDungeonService.cs
@@ -0,0 +1,11 @@
+﻿using Mde.Project.Mobile.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mde.Project.Mobile.Domain.Interfaces
+{
+    public interface IDungeonService : IEventService<DungeonModel>
+    {
+    }
+}
