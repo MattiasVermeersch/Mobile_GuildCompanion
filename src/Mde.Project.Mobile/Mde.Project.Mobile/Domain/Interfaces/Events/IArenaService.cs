e00797980ace544484cf555d8fcab485f920a249
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-20 23:08:27 +0100

    Create ArenaService interface

---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Interfaces/Events/IArenaService.cs
@@ -0,0 +1,12 @@
+﻿using Mde.Project.Mobile.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mde.Project.Mobile.Domain.Interfaces
+{
+    public interface IArenaService : IEventService<ArenaModel>
+    {
+    }
+}
