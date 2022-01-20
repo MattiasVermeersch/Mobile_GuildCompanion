e844e5c6d8c617e74fed9c32865ad56064669287
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-20 23:07:05 +0100

    Create BaseService interface

---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Interfaces/Base/IBaseService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mde.Project.Mobile.Domain.Interfaces
+{
+    public interface IBaseService<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(string id);
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(string id);
+    }
+}
