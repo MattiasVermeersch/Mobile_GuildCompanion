8c2381ef69a202c8610ff00507775e40dcd642bc
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-23 23:09:48 +0100

    Create EvenListItemModel

---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Models/Events/EventListItemModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mde.Project.Mobile.Domain.Models
+{
+    public class EventListItemModel : ModelBase
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Type { get; set; }
+    }
+}
