6d65b96d04e333b3250eb3b9394f975543cd3523
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-23 23:36:43 +0100

    Add DetailEventPage as subscription form for events

--- src/Mde.Project.Mobile/Mde.Project.Mobile/Pages/Events/DetailEventPage.xaml.cs
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Pages/Events/DetailEventPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Autofac;
+using Mde.Project.Mobile.Domain.Models;
+using Mde.Project.Mobile.IoC;
+using Mde.Project.Mobile.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,12 +13,24 @@ using Xamarin.Forms.Xaml;
 
 namespace Mde.Project.Mobile.Pages
 {
+    [QueryProperty(nameof(EventId), nameof(EventId))]
+    [QueryProperty(nameof(EventType), nameof(EventType))]
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DetailEventPage : ContentPage
     {
+        public string EventId { get; set; }
+        public string EventType { get; set; }
         public DetailEventPage()
         {
             InitializeComponent();
+            var viewModel = IoCResolver.Container.Resolve<EventDetailViewModel>();
+            BindingContext = viewModel;
+        }
+
+        protected override void OnAppearing()
+        {
+            var parameters = new EventParameters{ Id = EventId, Type = EventType };
+            (BindingContext as EventDetailViewModel).GetDetailCommand.ExecuteAsync(parameters);
         }
     }
 }
