ae8c699f834627b75a8d250a5812b51debb264ee
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-23 23:34:27 +0100

    Add CharacterDetailPage as form for character handling

---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Pages/Characters/CharacterDetailPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Autofac;
+using Mde.Project.Mobile.IoC;
+using Mde.Project.Mobile.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mde.Project.Mobile.Pages
+{
+    [QueryProperty(nameof(CharacterId), nameof(CharacterId))]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CharacterDetailPage : ContentPage
+    {
+        public string CharacterId { get; set; }
+        public CharacterDetailPage()
+        {
+            InitializeComponent();
+            var viewModel = IoCResolver.Container.Resolve<CharacterDetailViewModel>();
+            BindingContext = viewModel;
+        }
+
+        protected override void OnAppearing()
+        {
+            (BindingContext as CharacterDetailViewModel).GetDetailCommand.ExecuteAsync(CharacterId);
+        }
+    }
+}
