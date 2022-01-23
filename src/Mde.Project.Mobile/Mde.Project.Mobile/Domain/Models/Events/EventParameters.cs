fcda3645cd90063d7aa3ae1680fe6260e7c881d4
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-23 23:10:02 +0100

    Create EventParameters class

---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Models/Events/EventParameters.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mde.Project.Mobile.Domain.Models
+{
+    public class EventParameters
+    {
+        public string Id { get; set; }
+        public string Type { get; set; }
+    }
+}
