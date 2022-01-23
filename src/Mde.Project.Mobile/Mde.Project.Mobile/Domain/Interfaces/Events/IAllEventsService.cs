338d425e39ffc43fb63dd1f6bf0ca361e44ce16c
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-23 23:08:06 +0100

    Create IAllEventsService

---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Interfaces/Events/IAllEventsService.cs
@@ -0,0 +1,13 @@
+﻿using Mde.Project.Mobile.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Mde.Project.Mobile.Domain.Interfaces
+{
+    public interface IAllEventsService
+    {
+        Task<ICollection<EventListItemModel>> ListAllAsync();
+        Task<ICollection<EventListItemModel>> ListAllPvpAsync();
+        Task<ICollection<EventListItemModel>> ListAllPveAsync();
+    }
+}
