5f406e9bdea2674a93f6726337dc575d1c2cd128
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-23 23:10:14 +0100

    Create GlobalEventModel

---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Models/Events/GlobalEventModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mde.Project.Mobile.Domain.Models
+{
+    public class GlobalEventModel : EventModelBase
+    {
+        //Pvp props
+        public string Mode { get; set; }
+
+        //Pve props
+        //Dungeon props
+        public string Difficulty { get; set; }
+        public int? Key { get; set; }
+    }
+}
