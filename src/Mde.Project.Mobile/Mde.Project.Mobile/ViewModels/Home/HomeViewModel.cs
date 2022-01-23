d6ea0e626f9f722226cdb82e4b9abad1f73ba9de
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-23 23:28:06 +0100

    Update HomeViewModel to give overview of upcoming events

--- src/Mde.Project.Mobile/Mde.Project.Mobile/ViewModels/Home/HomeViewModel.cs
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/ViewModels/Home/HomeViewModel.cs
@@ -1,5 +1,9 @@
 ﻿using Autofac;
+using Mde.Project.Mobile.Domain.Interfaces;
+using Mde.Project.Mobile.Domain.Models;
 using Mde.Project.Mobile.IoC;
+using Mde.Project.Mobile.Pages;
+using MvvmHelpers.Commands;
 using Plugin.Toasts;
 using System;
 using System.Collections.Generic;
@@ -11,13 +15,58 @@ namespace Mde.Project.Mobile.ViewModels
 {
     public class HomeViewModel : ViewModelBase
     {
-        public HomeViewModel()
+        private readonly IAllEventsService _service;
+
+        public HomeViewModel(IAllEventsService service)
         {
+            _service = service;
             Title = "Home";
+            NotificationCommand = new AsyncCommand(ShowNotification);
+            SelectedCommand = new AsyncCommand<object>(Selected);
         }
 
         #region Properties
+        public int Count { get; set; }
 
+        private IEnumerable<EventListItemModel> pvpEvents;
+        public IEnumerable<EventListItemModel> PvpEvents
+        {
+            get => pvpEvents;
+            set => SetProperty(ref pvpEvents, value);
+        }
+
+        private IEnumerable<EventListItemModel> pveEvents;
+        public IEnumerable<EventListItemModel> PveEvents
+        {
+            get => pveEvents;
+            set => SetProperty(ref pveEvents, value);
+        }
+
+        private EventListItemModel selectedEvent;
+        public EventListItemModel SelectedEvent
+        {
+            get => selectedEvent;
+            set => SetProperty(ref selectedEvent, value);
+        }
+
+        private bool hasPveEvents;
+        public bool HasPveEvents
+        {
+            get => hasPveEvents;
+            set => SetProperty(ref hasPveEvents, value);
+        }
+
+        private bool hasPvpEvents;
+        public bool HasPvpEvents
+        {
+            get => hasPvpEvents;
+            set => SetProperty(ref hasPvpEvents, value);
+        }
+        #endregion
+
+        #region Commands
+        public AsyncCommand NotificationCommand { get; }
+        public AsyncCommand<object> SelectedCommand { get; }
         #endregion
 
         #region Methods
@@ -26,38 +75,92 @@ namespace Mde.Project.Mobile.ViewModels
             IsBusy = true;
 
             await base.GetData();
-            await ShowNotification();
+            await GetEventsCount();
+            await GetPveEventsList();
+            await GetPvpEventsList();
 
             IsBusy = false;
         }
 
+        private async Task GetEventsCount()
+        {
+            var events = await _service.ListAllAsync();
+
+            Count = events.Count;
+        }
+
+        private async Task GetPveEventsList()
+        {
+            var events = await _service.ListAllPveAsync();
+
+            if(events.Count == 0)
+            {
+                HasPveEvents = false;
+            }
+            else
+            {
+                HasPveEvents = true;
+                PveEvents = events;
+            }
+            await Task.CompletedTask;
+        }
+
+        private async Task GetPvpEventsList()
+        {
+            var events = await _service.ListAllPvpAsync();
+
+            if (events.Count == 0)
+            {
+                HasPvpEvents = false;
+            }
+            else
+            {
+                HasPvpEvents = true;
+                PvpEvents = events;
+            }
+            await Task.CompletedTask;
+        }
+
+        private async Task Selected(object args)
+        {
+            var eventItem = args as EventListItemModel;
+
+            if (eventItem is null) return;
+
+            var route = $"{nameof(DetailEventPage)}?EventId={eventItem.Id}&EventType={eventItem.Type}";
+            await Shell.Current.GoToAsync(route);
+        }
+
         async Task ShowNotification()
         {
-            var notificator = DependencyService.Get<IToastNotificator>();
-
-            //Description right now is a placeholder. When ApiService is implemented the relevant variables will be used.
-            var options = new NotificationOptions()
+            if (Count > 0)
             {
-                Title = "You have events today!",
-                Description = "Check it out!",
-                IsClickable = true,
-                WindowsOptions = new WindowsOptions()
+                var notificator = DependencyService.Get<IToastNotificator>();
+
+                //Description right now is a placeholder. When ApiService is implemented the relevant variables will be used.
+                var options = new NotificationOptions()
                 {
-                    LogoUri = "wow_icon.png"
-                },
-                ClearFromHistory = true,
-                AllowTapInNotificationCenter = false,
-                AndroidOptions = new AndroidOptions()
+                    Title = "You have events today!",
+                    Description = "Check it out!",
+                    IsClickable = true,
+                    WindowsOptions = new WindowsOptions()
+                    {
+                        LogoUri = "wow_icon.png"
+                    },
+                    ClearFromHistory = true,
+                    AllowTapInNotificationCenter = false,
+                    AndroidOptions = new AndroidOptions()
+                    {
+                        HexColor = "#8cde95",
+                        ForceOpenAppOnNotificationTap = true
+                    }
+                };
+
+                var result = await notificator.Notify(options);
+                if (result.Action == NotificationAction.Clicked)
                 {
-                    HexColor = "#8cde95",
-                    ForceOpenAppOnNotificationTap = true
+                    await Shell.Current.GoToAsync($"{nameof(PersonalEventsPage)}");
                 }
-            };
-
-            var result = await notificator.Notify(options);
-            if(result.Action == NotificationAction.Clicked)
-            {
-                await Shell.Current.DisplayAlert("Alert","You clicked to see your events.", "OK");
             }
         }
         #endregion
