6b657808254cc7241fe60f7b1b281adfe879ed57
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-23 23:37:15 +0100

    Add PersonalEventsPage to provide overview of subscribed events for user

---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Pages/Events/PersonalEventsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Autofac;
+using Mde.Project.Mobile.IoC;
+using Mde.Project.Mobile.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mde.Project.Mobile.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PersonalEventsPage : ContentPage
+    {
+        public PersonalEventsPage()
+        {
+            InitializeComponent();
+
+            var viewModel = IoCResolver.Container.Resolve<PersonalEventsViewModel>();
+
+            BindingContext = viewModel;
+        }
+
+        protected override void OnAppearing()
+        {
+            (BindingContext as PersonalEventsViewModel).RefreshCommand.ExecuteAsync();
+        }
+    }
+}
