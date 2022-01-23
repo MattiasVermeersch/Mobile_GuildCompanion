ab6ab1c96029f4a0df1fba5dca915f9d6e95395b
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-23 23:23:50 +0100

    Create converters to go boolean

---
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Converters/EmptyCollectionToBoolConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Mde.Project.Mobile.Domain.Converters
+{
+    public class EmptyCollectionToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return false;
+
+            return !((IEnumerable<object>)value).Any();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
