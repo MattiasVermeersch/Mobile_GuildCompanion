c5cb65c3d6da923b1685a8edb1e182a834b7adc9
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-23 23:14:15 +0100

    Update Api Character Service

--- src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Services/ApiServices/Characters/ApiCharacterService.cs
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/Domain/Services/ApiServices/Characters/ApiCharacterService.cs
@@ -1,17 +1,47 @@
 ﻿using Mde.Project.Mobile.Domain.Constants;
 using Mde.Project.Mobile.Domain.Interfaces;
 using Mde.Project.Mobile.Domain.Models;
+using Mde.Project.Mobile.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
 
 namespace Mde.Project.Mobile.Domain.Services
 {
     public class ApiCharacterService : BaseService<CharacterModel>, ICharactersService
     {
+        protected string _usersUrl; 
         public ApiCharacterService()
         {
             _url = ApiConstants.ApiCharactersUrl;
+            _usersUrl = ApiConstants.ApiUsersUrl;
+        }
+
+        public async Task<IEnumerable<CharacterModel>> GetCharactersByUserIdAsync()
+        {
+            await GetStorageUserId();
+            var userCharacters = await ApiClient.GetAsync<IEnumerable<CharacterModel>>($"{_usersUrl}/{_userId}/{ApiConstants.Characters}");
+            return userCharacters;
+        }
+
+        public override async Task<CharacterModel> AddAsync(CharacterModel entity)
+        {
+            var characterRequest = new
+            {
+                entity.Name,
+                entity.Class,
+                entity.Role,
+                entity.Level,
+                entity.ItemLevel,
+                entity.ArenaRating,
+                entity.BattlegroundRating,
+                entity.UserId
+            };
+
+            var response = await ApiClient.PostAsync<CharacterModel>(_url, characterRequest);
+            return response;
         }
     }
 }
