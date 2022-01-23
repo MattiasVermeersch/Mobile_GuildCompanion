148d9f72c0460f0da183501ed5014cf8a13f76eb
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-23 23:24:27 +0100

    Update CharacterViewModel to go to Detail page

--- src/Mde.Project.Mobile/Mde.Project.Mobile/ViewModels/Characters/CharacterViewModel.cs
+++ src/Mde.Project.Mobile/Mde.Project.Mobile/ViewModels/Characters/CharacterViewModel.cs
@@ -11,11 +11,16 @@ namespace Mde.Project.Mobile.ViewModels
     public class CharacterViewModel : ViewModelBase
     {
         private readonly ICharactersService _characterService;
+        public AsyncCommand<object> SelectedCommand { get; }
+        public AsyncCommand AddCommand { get; }
 
         public CharacterViewModel(ICharactersService characterService)
         {
             _characterService = characterService;
             Title = "My Characters";
+
+            SelectedCommand = new AsyncCommand<object>(Selected);
+            AddCommand = new AsyncCommand(ToCreatePage);
         }
 
         #region Properties
@@ -30,11 +35,21 @@ namespace Mde.Project.Mobile.ViewModels
         public CharacterModel SelectedCharacter
         {
             get => selectedCharacter;
-            set
-            {
-                Shell.Current.DisplayAlert("Selected", value.Id, "OK");
-                SetProperty(ref selectedCharacter, value);
-            }
+            set => SetProperty(ref selectedCharacter, value);
+        }
+
+        private bool hasData;
+        public bool HasData
+        {
+            get => hasData;
+            set => SetProperty(ref hasData, value);
+        }
+
+        private bool hasNoData;
+        public bool HasNoData
+        {
+            get => hasNoData;
+            set => SetProperty(ref hasNoData, value);
         }
         #endregion
 
@@ -51,7 +66,35 @@ namespace Mde.Project.Mobile.ViewModels
 
         public async Task GetCharactersList()
         {
-            Characters = await _characterService.GetAllAsync();
+            var characters = await _characterService.GetCharactersByUserIdAsync();
+            if(characters == null)
+            {
+                HasNoData = true;
+                HasData = false;
+            }
+            else
+            {
+                Characters = characters;
+                HasNoData = false;
+                HasData = true;
+            }
+        }
+
+        private async Task Selected(object args)
+        {
+            var character = args as CharacterModel;
+
+            if (character is null) return;
+
+            var route = $"{nameof(CharacterDetailPage)}?CharacterId={character.Id}";
+            await Shell.Current.GoToAsync(route);
+        }
+
+        private async Task ToCreatePage()
+        {
+            //do not pass a CharacterId to the page, this will set page to create mode
+            var route = $"{nameof(CharacterDetailPage)}";
+            await Shell.Current.GoToAsync(route);
         }
         #endregion
     }
